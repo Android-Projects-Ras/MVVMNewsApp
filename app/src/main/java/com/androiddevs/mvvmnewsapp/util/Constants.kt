d3d2bda58dae5f68ab5905a0da169769fb2158bc
Andrew Rozhkov <buhhater@gmail.com>
2021-02-01 16:01:27 +0200

    searchNewsFragment setup

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
     companion object{
         const val API_KEY = "561aec31d82b46a88ceae556bdad766a"
         const val BASE_URL = "http://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
