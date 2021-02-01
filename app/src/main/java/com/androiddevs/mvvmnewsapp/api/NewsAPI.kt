6f9e893140bb1709e96af9f22d431a8fcc01f1e7
Andrew Rozhkov <buhhater@gmail.com>
2021-02-01 14:59:09 +0200

    handling breaking news response

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
@@ -0,0 +1,32 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
+import com.androiddevs.mvvmnewsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//http://newsapi.org/v2/top-headlines?country=ua&apiKey=561aec31d82b46a88ceae556bdad766a
+
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "ua",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ) : Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ) : Response<NewsResponse>
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "561aec31d82b46a88ceae556bdad766a"
+        const val BASE_URL = "http://newsapi.org"
+    }
+}
